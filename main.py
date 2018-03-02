b27ccc019204748b75db1e617ee11c49a2247605
Masayuki Komatsu <info@notsleeeping.com>
2018-03-02 11:33:10 +0900

    refactor func

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from tool.rot import rot
+import codecs
 
 string = "EBG KVVV vf n fvzcyr yrggre fhofgvghgvba pvcure gung ercynprf n yrggre jvgu gur yrggre KVVV yrggref nsgre vg va gur nycunorg. EBG KVVV vf na rknzcyr bs gur Pnrfne pvcure, qrirybcrq va napvrag Ebzr. Synt vf SYNTFjmtkOWFNZdjkkNH. Vafreg na haqrefpber vzzrqvngryl nsgre SYNT."
 
 print(rot(13, string))
+
+res = codecs.decode(string, "rot13")
+
+
+print(res)
--- tool/rot.py
+++ tool/rot.py
@@ -4,14 +4,14 @@ def rot(n, str):
     arr = []
     for s in str:
         x = ord(s)
-        if 65 < x & x < 90:
+        if 65 <= x & x <= 90:
             x = x + n
-            if x > 90: x = x - 26
+            if x > 90:
+                x = x - 26
 
-        elif 97 < x & x < 122:
+        elif 97 <= x & x <= 122:
             x = x + n
-            if x > 122: x = x - 26
-
+            if x > 122:
+                x = x - 26
         arr.append(chr(x))
-
     return "".join(arr)
